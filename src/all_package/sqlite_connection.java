dd781d547848a5e69dca9502e87304983ef9054b
oSikaNi iSraeL <oSikaNi iSraeL@oSikaNiiSraeL>
2017-08-02 16:33:10 +0000

    sqlite integration

---
+++ src/all_package/sqlite_connection.java
@@ -0,0 +1,11 @@
+
+package all_package;
+
+import java.sql.*;
+import java.sql.Connection;
+public class sqlite_connection {
+    
+    private static Connection con;
+    private static boolean hasData=false;
+    
+}

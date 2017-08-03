df6f0f87aa737c237a197b70a3491a9dae83442e
oSikaNi iSraeL <oSikaNi iSraeL@oSikaNiiSraeL>
2017-08-03 20:23:32 +0000

    sqlite integration

--- src/all_package/Admin_BirdPage.java
+++ src/all_package/Admin_BirdPage.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 import net.proteanit.sql.DbUtils;
 
 /**
@@ -26,6 +27,8 @@ public class Admin_BirdPage extends javax.swing.JFrame {
      */
     public Admin_BirdPage() {
         initComponents();
+        
+        bird_id.setVisible(false);
         conn = java_Connection.ConnecrDb();
        Update_table();
     }
@@ -63,7 +66,7 @@ public class Admin_BirdPage extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        cage_no = new javax.swing.JTextField();
         jComboBox1 = new javax.swing.JComboBox<>();
         jLabel3 = new javax.swing.JLabel();
         jComboBox2 = new javax.swing.JComboBox<>();
@@ -78,14 +81,15 @@ public class Admin_BirdPage extends javax.swing.JFrame {
         jLabel10 = new javax.swing.JLabel();
         jLabel11 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jComboBox3 = new javax.swing.JComboBox<>();
-        jComboBox4 = new javax.swing.JComboBox<>();
-        jComboBox5 = new javax.swing.JComboBox<>();
-        jComboBox6 = new javax.swing.JComboBox<>();
+        collor = new javax.swing.JTextField();
+        Pen_no = new javax.swing.JTextField();
+        bre_type = new javax.swing.JComboBox<>();
+        bird_Type = new javax.swing.JComboBox<>();
+        sex_ = new javax.swing.JComboBox<>();
+        category = new javax.swing.JComboBox<>();
         jDateChooser1 = new com.toedter.calendar.JDateChooser();
         jButton3 = new javax.swing.JButton();
+        bird_id = new javax.swing.JTextField();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -153,10 +157,15 @@ public class Admin_BirdPage extends javax.swing.JFrame {
         jButton5.setForeground(new java.awt.Color(255, 255, 255));
         jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/Edit Property_25px.png"))); // NOI18N
         jButton5.setText("Update");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 80, -1, -1));
 
-        jTextField1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 490, 80, 30));
+        cage_no.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jPanel1.add(cage_no, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 490, 80, 30));
 
         jComboBox1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         jComboBox1.setForeground(new java.awt.Color(255, 255, 255));
@@ -185,6 +194,11 @@ public class Admin_BirdPage extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
 
         jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, 1030, 290));
@@ -240,27 +254,27 @@ public class Admin_BirdPage extends javax.swing.JFrame {
         jTextField2.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 80, 170, 40));
 
-        jTextField3.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jPanel1.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 440, 140, 30));
+        collor.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jPanel1.add(collor, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 440, 140, 30));
 
-        jTextField4.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jPanel1.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 440, 80, 30));
+        Pen_no.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jPanel1.add(Pen_no, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 440, 80, 30));
 
-        jComboBox3.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox3, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 490, 140, 30));
+        bre_type.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        bre_type.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Ancona", "Araucana", "Barnevelder", "Black Rock", "Brahma", "Cochin", "Cream Legbar", "Dorking", "Dutch Partridge", "Hybrids", "Japanese Bantam (Chabo)", "Marans", "Marsh Daisy", "Modern Game", "Old English Game", "Orpington", "Pekin (Lemon Cuckoo)", "Poland", "Rhode Island Red", "Seabright", "Silkie", "Speckeldy", "Sultan", "Sussex Light" }));
+        jPanel1.add(bre_type, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 490, 140, 30));
 
-        jComboBox4.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jComboBox4.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox4, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 440, 140, 30));
+        bird_Type.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        bird_Type.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Chicken", "Turkey", "Duck", "Geese", "Pigeon", "Guinea", "Fowl", "Quail", "Ostrich", "Emu" }));
+        jPanel1.add(bird_Type, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 440, 140, 30));
 
-        jComboBox5.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jComboBox5.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox5, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 490, 150, 30));
+        sex_.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        sex_.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Female", "Male" }));
+        jPanel1.add(sex_, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 490, 150, 30));
 
-        jComboBox6.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jComboBox6.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox6, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 440, 150, 30));
+        category.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        category.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Category A", "Category B", "Category C", "Category D" }));
+        jPanel1.add(category, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 440, 150, 30));
         jPanel1.add(jDateChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 490, 140, 30));
 
         jButton3.setBackground(new java.awt.Color(8, 8, 45));
@@ -278,6 +292,7 @@ public class Admin_BirdPage extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(970, 440, -1, 90));
+        jPanel1.add(bird_id, new org.netbeans.lib.awtextra.AbsoluteConstraints(940, 80, 90, 30));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1070, 550));
 
@@ -330,6 +345,7 @@ public class Admin_BirdPage extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        Update_table();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
@@ -368,6 +384,87 @@ public class Admin_BirdPage extends javax.swing.JFrame {
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+        // TODO add your handling code here:
+        
+          try{
+
+            int row = jTable1.getSelectedRow();
+            String Table_click=(jTable1.getModel().getValueAt(row,0).toString());
+
+            String sql ="SELECT * FROM new_bird WHERE  bird_id = '"+Table_click+"'";
+            pst=conn.prepareStatement(sql);
+            rs=pst.executeQuery();
+
+            if(rs.next()){
+
+                String id = rs.getString("bird_id");
+                bird_id.setText(id);
+                String pen = rs.getString("pen_number");
+                Pen_no.setText(pen);
+                String cage = rs.getString("cage_number");
+                cage_no.setText(cage);
+                String bird = rs.getString("bird_type");
+                bird_Type.setSelectedItem(bird);
+
+                String breed =rs.getString("breed_type");
+                bre_type.setSelectedItem(breed);
+
+                String cat = rs.getString("bird_Cate");
+                category.setSelectedItem(cat);
+
+                String sex = rs.getString("gender");
+                sex_.setSelectedItem(sex);
+
+                String color =rs.getString("color");
+                collor.setText(color);
+
+            }
+
+        }
+        catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }//GEN-LAST:event_jTable1MouseClicked
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+        
+        int row=jTable1.getSelectedRow();
+
+        if(row==-1){
+
+            JOptionPane.showMessageDialog(null, " Please Select a Row and Update Where Neccessary");
+
+        }
+        else{
+            try{
+                String value1=bird_id.getText();
+                String value2=Pen_no.getText();
+                String value3=cage_no.getText();
+                String value4=(String) bird_Type.getSelectedItem();
+                String value5=(String) bre_type.getSelectedItem();
+                String value6=(String) category.getSelectedItem();
+                String value7=(String) category.getSelectedItem();
+                String value8=collor.getText();
+                String value9=((JTextField)jDateChooser1.getDateEditor().getUiComponent()).getText();
+
+                String sql ="update new_bird set bird_id='"+value1+"', pen_number='"+value2+"', cage_number='"+value3+"', bird_type='"+value4+"', breed_type='"+value5+"', bird_Cate='"+value6+"', gender='"+value7+"', color='"+value8+"', date_added='"+value9+"' where bird_id='"+value1+"'";
+
+                pst=conn.prepareStatement(sql);
+                pst.execute();
+                JOptionPane.showMessageDialog(null, "Update Successful");
+            }
+
+            catch(Exception e){
+
+                JOptionPane.showMessageDialog(null, e);
+            }
+
+        }
+
+    }//GEN-LAST:event_jButton5ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -404,6 +501,13 @@ public class Admin_BirdPage extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField Pen_no;
+    private javax.swing.JComboBox<String> bird_Type;
+    private javax.swing.JTextField bird_id;
+    private javax.swing.JComboBox<String> bre_type;
+    private javax.swing.JTextField cage_no;
+    private javax.swing.JComboBox<String> category;
+    private javax.swing.JTextField collor;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -411,10 +515,6 @@ public class Admin_BirdPage extends javax.swing.JFrame {
     private javax.swing.JButton jButton5;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JComboBox<String> jComboBox2;
-    private javax.swing.JComboBox<String> jComboBox3;
-    private javax.swing.JComboBox<String> jComboBox4;
-    private javax.swing.JComboBox<String> jComboBox5;
-    private javax.swing.JComboBox<String> jComboBox6;
     private com.toedter.calendar.JDateChooser jDateChooser1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
@@ -436,9 +536,7 @@ public class Admin_BirdPage extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
+    private javax.swing.JComboBox<String> sex_;
     // End of variables declaration//GEN-END:variables
 }
